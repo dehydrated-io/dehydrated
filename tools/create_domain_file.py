43d56643677edea76a2983eedf47db82779d9e19
Aaron Cowdin <aaron.cowdin@gmail.com>
2016-03-29 13:48:59 -0700

    Add patterns domain to cert.

--- tools/create_domain_file.py
+++ tools/create_domain_file.py
@@ -19,6 +19,7 @@ def subdomain_from_repo(repo):
         'toychest': "store",
         'lightyear': "room",
         'techcheck': "setup",
+        'pl-docs': "patterns",
     }
 
     return switcher.get(repo, "nothing")
@@ -35,7 +36,7 @@ def create_domain_string(domain, subdomain, new_treeish):
 
     s3_bucket_list = s3_bucket.list(prefix="deploy/" + flavor + "-app/deployed_hashes/")
 
-    deployed_list = [domain, 'kidinsight.' + domain, 'room.' + domain, 'store.' + domain, 'apps.' + domain, 'setup.' + domain, 'login.' + domain, 'test.' + domain, 'live.' + domain, 'kibana.' + domain]
+    deployed_list = [domain, 'kidinsight.' + domain, 'room.' + domain, 'store.' + domain, 'apps.' + domain, 'setup.' + domain, 'login.' + domain, 'test.' + domain, 'live.' + domain, 'kibana.' + domain, 'patterns.' + domain]
     return " ".join(deployed_list)
 
     deployed_set = set({})
