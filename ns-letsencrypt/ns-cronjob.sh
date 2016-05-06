80a143caa41e67023136c1e9561c299318374a25
root <root@vrobot.lab.local>
2016-05-06 09:36:10 -0500

    added

--- ns-letsencrypt/ns-cronjob.sh
+++ ns-letsencrypt/ns-cronjob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #use this line to force renewal. USE WITH CAUTION /root/letsencrypt.sh/letsencrypt.sh -c -x -k /root/letsencrypt.sh/ns-letsencrypt/ns-hook.sh
-/root/letsencrypt.sh/letsencrypt.sh -c -x -k /root/letsencrypt.sh/ns-letsencrypt/ns-hook.sh
+/root/letsencrypt.sh/letsencrypt.sh -c -k /root/letsencrypt.sh/ns-letsencrypt/ns-hook.sh
 /root/letsencrypt.sh/letsencrypt.sh -gc
