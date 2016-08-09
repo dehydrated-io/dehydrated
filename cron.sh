a35bb7537238689853afd66d53f1a3e42263782f
Gideon Farrell <gideon@converge.io>
2016-08-09 12:33:35 +0100

    add cron script

---
+++ cron.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $(cd $(dirname $0) && pwd));
+
+$DIR/letsencrypt.sh \
+    --cron \
+    --challenge dns-01 \
+    --hook "$DIR/hooks/cloudflare/hook.py" \
+    --out "/etc/secure/converge/"
